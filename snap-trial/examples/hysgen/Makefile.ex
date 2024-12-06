f83cd3480153a8d00ff87e1dc99bd88181c36c32
oscar.otero2 <oscar.otero2@udc.es>
2024-12-06 04:00:00 +0100

    Eliminadas más dependencias

--- snap-trial/examples/hysgen/Makefile.ex
+++
@@ -1,9 +0,0 @@
-## Main application file
-MAIN = hysgen_main
-DEPH = $(EXSNAPADV)/hysgen.h
-DEPCPP = $(EXSNAPADV)/hysgen.cpp
-#CXXFLAGS += $(CXXOPENMP)
-#CXXFLAGS += -g -rdynamic
-#CXXFLAGS += -ggdb
-#CXXFLAGS += -ggdb3 -rdynamic
-
